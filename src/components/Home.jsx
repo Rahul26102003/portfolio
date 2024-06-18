1af9e2172836dfc476ec7ff5c9b21be1d9d3be6a
Rahul26102003 <2221032cse.coe@cgc.edu.in>
2024-06-18 11:03:31 +0530

    Created a change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,8 @@ import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800  sm:pt-[800px] sm:pb-[200px] md:p-0'>
-        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 sm:pt-[300px] sm:pb-[300px] md:p-0'>
+        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row pt-[150px]'>
             <div className='flex flex-col justify-center h-full'>
                 <h2 className='text-4xl text-white sm:text-7xl font-bold'>I'm a Full Stack Developer</h2>
                 <p className='text-gray-500 py-4 max-w-md'>I have 2 years of experience building and designing software. Currently, I love to work on web application using technologies like React, Tailwind, Next JS and GraphQL.</p>
@@ -18,7 +18,7 @@ const Home = () => {
                 </Link>
             </div>
             </div>
-            <div className='sm:pt-[300px] md:p-0'>
+            <div className='pt-[60px] md:p-0'>
                 <img src={HeroImage} alt="my profile" className='rounded-2xl h-[330px]  w-[360px] mx-auto md:w-[450px] hover:scale-105 duration-300'/>
             </div>
         </div>
